afd2c263b02b61039e48b903831b5fcbc468a394
Leo <ss0147532097@gmail.com>
2023-05-31 11:33:45 +0800

    調整util

--- src/main/java/tw/com/imsoft/utils/PostApiUtil.java
+++ src/main/java/tw/com/imsoft/utils/PostApiUtil.java
@@ -12,18 +12,16 @@ public final class PostApiUtil {
 
     public static JsonNode sendPost(String channelId,String nonce, String signature,String httpsUrl,String mapperData) {
         RestTemplate restTemplate = new RestTemplate();
-        JsonNode json = null;
 //      Post Headers 設定
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.add("X-LINE-ChannelId", channelId);
         headers.add("X-LINE-Authorization-Nonce", nonce);
         headers.add("X-LINE-Authorization", signature);
-        HttpEntity<String> request = 
-                new HttpEntity<String>(mapperData, headers);
-        String responsebody
-          = restTemplate.postForObject(httpsUrl, request,String.class);
+        HttpEntity<String> request = new HttpEntity<String>(mapperData, headers);
+        String responsebody = restTemplate.postForObject(httpsUrl, request,String.class);
         ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = null;
         try {
             json = mapper.readTree(responsebody);
             return json;
